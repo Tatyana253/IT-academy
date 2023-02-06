5c0a4d2146fdc4bc203b65c1a2e37d6445d35361
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-02-06 22:00:10 +0100

    Lesson7Commit

---
+++ newFileLesson.js
@@ -0,0 +1,30 @@
+let bool = true;
+let num = 5;
+let str = "string";
+
+let sumc = str + bool;
+let sume = str + num;
+let sumd = num + bool;
+
+
+let multa = str * bool;
+let multb = str * num;
+let multc = num * bool;
+
+
+let diva = str / bool;
+let divb = str / num;
+let divc = num / bool;
+
+
+parseInt('22@@');
+
+
+let numa = 10
+let text = numa.toString(2);
+
+
+let boolnew = true;
+let textnew = boolnew.toString();
+
+
