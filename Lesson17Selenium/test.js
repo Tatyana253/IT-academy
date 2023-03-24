9a4aa87823acd50e6af02b03fa7af11c720dc536
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-03-24 23:25:10 +0100

    update

--- Lesson17Selenium/test.js
+++ Lesson17Selenium/test.js
@@ -1,21 +1,38 @@
-const {Builder, By, until } = require('selenium-webdriver');
+const {Builder, By, Key, until } = require("selenium-webdriver");
+const { expect } = require("chai");
 
-(async function () {
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('https://chromedriver.chromium.org/home');
-    const mainTitle = await driver.findElement(By.className('Rn3Z1b C9DxTc'));
+
+describe('ChromeDriver Website test', function () {
+    let driver;
+    before(async () => {
+       driver = await new Builder().forBrowser('chrome').build();
+
+    });
+
+
+    after(async () => {
+        await driver.quit();
+    });
+
+  it("text should equal Chrome driver", async () => {
+
+    await driver.get("https://chromedriver.chromium.org/home");
+    const mainTitle = await driver.findElement(By.className("Rn3Z1b C9DxTc"));
     let text = await mainTitle.getText();
-    if (text === 'ChromeDriver') {
-        console.log('Text is "ChromeDriver"');
-      } else {
-        console.log(`Text is not "ChromeDriver". It is: ${text}`);
-      } 
-      const chromeExtensions = await driver.findElement(By.xpath("//*[text()='Chrome Extensions']"));
-      await driver.wait(until.elementIsVisible(By.xpath("//*[text()='Chrome Extensions']")));
-      await chromeExtensions.click();
+    expect(await title.toLowerCase()).to.contain('chromedriver');
+  });
+
+  it("go to chrome extensions", async () => {
+    await driver.get("https://chromedriver.chromium.org/home");
+    await driver.wait(until.elementIsVisible(await driver.findElement(
+        By.xpath("//*[@class='aJHbb hDrhEe HlqNPb'] [@data-url='/extensions']"))), 10000);
+    const chromeExtensions = await driver.findElement(
+      By.xpath("//*[@class='aJHbb hDrhEe HlqNPb'] [@data-url='/extensions']")
+    );
+    await chromeExtensions.click();
+    await driver.wait(until.urlContains("/extensions"), 3000);
+
+  });
 
 
-    await driver.sleep(3000);
-    await driver.quit();
-}
-)();
+});
