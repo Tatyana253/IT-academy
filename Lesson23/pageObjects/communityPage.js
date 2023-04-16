109037f142c6d8aaa1a2bb9b184fa766152580fc
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-04-16 23:46:15 +0300

    update

---
+++ Lesson23/pageObjects/communityPage.js
@@ -0,0 +1,9 @@
+class CommunityPage {
+    constructor(page) {
+        this.page = page;
+        this.title = "//div[@class='theme-doc-markdown markdown']/h1";
+    }
+}
+
+module.exports = CommunityPage;
+
--- Lesson23/tests/testMy.spec.js
+++ Lesson23/tests/testMy.spec.js
@@ -2,21 +2,39 @@ const { test } = require('@playwright/test');
 const MainPage = require('../pageObjects/mainPage');
 const Header = require('../pageObjects/pageComponents/header');
 const DocPage = require('../pageObjects/docPage');
+const ApiPage = require('../pageObjects/apiPage');
+const CommunityPage = require('../pageObjects/communityPage');
 const {expect} = require('chai');
 
 test.describe('test Playwright website', async function() {
     let mainPage;
     let header;
     let docPage;
+    let apiPage;
+    let communityPage;
     test.beforeEach(async ({page}) => {
         mainPage = new MainPage(page);
         header = new Header(page);
         docPage = new DocPage(page);
+        communityPage = new CommunityPage(page);
+        apiPage = new ApiPage(page);
     });
 
     test('should navigate to Docs page', async ({page}) => {
         await mainPage.navigate('https://playwright.dev/');
         await header.goToPageWithTopNavMenu(header.docsLink);
         expect(await docPage.page.textContent(docPage.title)).to.equal('Installation');
+    });
+
+    test('should navigate to API page', async ({page}) => {
+        await mainPage.navigate('https://playwright.dev/');
+        await header.goToPageWithTopNavMenu(header.apiLink);
+        expect(await apiPage.page.textContent(apiPage.title)).to.equal('Playwright Library');
+    });
+
+    test('should navigate to Community page', async ({page}) => {
+        await mainPage.navigate('https://playwright.dev/');
+        await header.goToPageWithTopNavMenu(header.communityLink);
+        expect(await communityPage.page.textContent(communityPage.title)).to.equal('Welcome');
     });
 })
