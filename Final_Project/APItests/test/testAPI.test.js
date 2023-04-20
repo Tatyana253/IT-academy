88d4e548e9066559ba738397c31516ef50f877e0
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-04-20 22:25:00 +0300

    apiTests

--- Final_Project/APItests/test/testAPI.test.js
+++ Final_Project/APItests/test/testAPI.test.js
@@ -5,38 +5,38 @@ const getPublicConfigForContextSchema = require('../test/testData/getPublicConfi
 
 
 describe(`API tests for Decathlon translations`, function () {
-            describe('first test', function () {
-                let response;
-                beforeAll(async () => {
-                    response = await axios.get('https://www.decathlon.pl/pl/ajax/nfs/translations')
-                });
+    describe('first test', function () {
+        let response;
+        beforeAll(async () => {
+            response = await axios.get('https://www.decathlon.pl/pl/ajax/nfs/translations')
+        });
 
-                test('status code should be 200 for Decathlon translations', async () => {
-                    expect(response.status).toEqual(200);
+        test('status code should be 200 for Decathlon translations', async () => {
+            expect(response.status).toEqual(200);
+        })
+        test('json schema should be valid for Decathlon translations', async () => {
+            const result = await validator.validate(response.data, translationsSchema)
+            expect(result.valid).toEqual(true)
+        });
+
+
+        describe('API tests for getPublicConfigForContext on Decathlon', function () {
+            let responseTwo;
+            beforeAll(async () => {
+                responseTwo = await axios({
+                    method: "GET",
+                    url: 'https://synerise.decathlon.pl/rtom-proxy/rtom/configs/getPublicConfigForContext?tracker=576B50AE-A9AA-F66D-5161-26EC6204D9C2'
                 })
-                test('json schema should be valid for Decathlon translations', async () => {
-                    const result = await validator.validate(response.data, translationsSchema)
-                    expect(result.valid).toEqual(true)
-                });
+            });
 
+            test('status code should be 200 getPublicConfigForContext on Decathlon', async () => {
+                expect(responseTwo.status).toEqual(200)
+            })
 
-                describe('API tests for getPublicConfigForContext on Decathlon', function () {
-                    let responseTwo;
-                    beforeAll(async () => {
-                        responseTwo = await axios({
-                            method: "GET",
-                            url: 'https://synerise.decathlon.pl/rtom-proxy/rtom/configs/getPublicConfigForContext?tracker=576B50AE-A9AA-F66D-5161-26EC6204D9C2'
-                        })
-                    });
-
-                    test('status code should be 200 getPublicConfigForContext on Decathlon', async () => {
-                        expect(responseTwo.status).toEqual(200)
-                    })
-
-                    test('json schema should be valid for getPublicConfigForContext on Decathlon ', async () => {
-                        const resultTwo = await validator.validate(responseTwo.data, getPublicConfigForContextSchema);
-                        expect(resultTwo.valid).toEqual(true)
-                    })
-                })
+            test('json schema should be valid for getPublicConfigForContext on Decathlon ', async () => {
+                const resultTwo = await validator.validate(responseTwo.data, getPublicConfigForContextSchema);
+                expect(resultTwo.valid).toEqual(true)
             })
         })
+    })
+})
