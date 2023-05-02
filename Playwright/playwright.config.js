1fe7d2a7e9869206a4e83ed6a774a410c71f8cd2
Tatjana Danchenko <tatjana_danchenko@epam.com>
2023-05-02 18:47:15 +0300

    new

--- Playwright/playwright.config.js
+++ Playwright/playwright.config.js
@@ -1,4 +1,4 @@
-// @ts-check
+
 const { defineConfig, devices } = require('@playwright/test');
 
 /**
@@ -41,36 +41,6 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-/*
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-*/
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
 });
